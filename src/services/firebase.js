cd6def6f712ee41fbc61e271aa06d4327224b2fc
Falak Amin <falakamin2000@gmail.com>
2021-10-15 17:02:00 +0500

    Implemented Google Authentication Functionality

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,29 @@ const firebaseConfig = {
     messagingSenderId: "1053171955446",
     appId: "1:1053171955446:web:3c5f414bce2abf67491ed7"
   };
+  const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
