64a178033f11b390413679ad87d96eeed5363a08
Falak Amin <falakamin2000@gmail.com>
2021-10-15 16:53:49 +0500

    Installed Dependencies and Firebase Configuration

---
+++ src/services/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyCJGcwshhsoUp8bxLnNpzORgS8HXH5XiFw",
+    authDomain: "login-4f020.firebaseapp.com",
+    projectId: "login-4f020",
+    storageBucket: "login-4f020.appspot.com",
+    messagingSenderId: "1053171955446",
+    appId: "1:1053171955446:web:3c5f414bce2abf67491ed7"
+  };
